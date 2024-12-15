2d39231402e82aa0f5e32bf98c4221505c338f9d
faisallyt <fdiledu@gmail.com>
2024-12-16 01:15:57 +0530

    done

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import FaviconIcon from "../public/FaisalDilshadFaviconIcon.jpg";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Faisal Dilshad's Portfolio",
+  description: "A Complete Whatabout Faisal Dilshad",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/FaisalDilshadFaviconIcon.jpg" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/Clients.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+import { companies, testimonials } from "@/data";
+import { InfiniteMovingCards } from "./ui/InfiniteCards";
+import { MacbookScroll } from "./ui/macbook-scroll";
+
+const Clients = () => {
+  return (
+    <section id="testimonials" className="md:pt-[10rem] md:pb-[47rem]">
+      <MacbookScroll src="" />
+    </section>
+  );
+};
+
+export default Clients;
